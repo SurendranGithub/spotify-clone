27f2c3ef227ed2d9e36de294088ccaa20a525696
SurendranGithub <prasannarajan2107@gmail.com>
2024-07-09 18:20:26 +0530

    Website Complete

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react'
+import Navbar from './components/Navbar'
+import Player from './components/Player'
+import Display from './components/Display'
+import { PlayerContext } from './context/PlayerContext'
+
+
+const App = () => {
+
+  const {audioRef, track} = useContext(PlayerContext);
+
+  return (
+    <div className='h-screen bg-black'>
+      <div className='h-[90%] flex'>
+        <Navbar />
+        <Display />
+      </div>
+      <Player/>
+      <audio ref={audioRef}  src={track.file} preload='auto'></audio>
+    </div>
+  )
+}
+
+export default App
+
